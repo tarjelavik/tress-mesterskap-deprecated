70eb32ebd9e0abc48b6e3ae63f64ad35b1fb774e
Tarje Lavik <tarje.lavik@gmail.com>
2020-08-25 20:51:24 +0200

    Endrer undertittel

--- web/components/more-stories.js
+++ web/components/more-stories.js
@@ -4,7 +4,7 @@ export default function MoreStories({ matches }) {
   return (
     <section>
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        Flere kamper...
+        Flere slag...
       </h2>
       <p>{matches.results}</p>
       <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
