4f816f3072a1e4876f926862e2129cde649f665e
Tarje Lavik <tarje.lavik@gmail.com>
2020-12-25 18:46:36 +0100

    Fix error in graph?

--- web/components/score-graph.js
+++ web/components/score-graph.js
@@ -1,7 +1,7 @@
 import { Line } from "react-chartjs-2";
 import { getResultScoreSeries } from "../lib/functions";
 
-export default function ScoreGraph({ player, games }) {
-  const data = getResultScoreSeries(player, games);
+export default function ScoreGraph({ player, matches }) {
+  const data = getResultScoreSeries(player, matches);
   return <Line data={data} width={400} height={200} />;
 }
