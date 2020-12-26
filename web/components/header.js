45b68f2fbfa522f8e1976ccda90098c7d5222be4
Tarje Lavik <tarje.lavik@gmail.com>
2020-12-26 14:24:26 +0100

    Add responsive menu

--- web/components/header.js
+++ web/components/header.js
@@ -1,22 +1,53 @@
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Header() {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
   return (
     <header>
-      <nav className="flex justify-between w-full pt-4 pb-4">
+      <nav className="flex items-center flex-wrap bg-yellow-500 p-3 mb-12">
         <a href="/">
-          <span className="font-semibold text-xl tracking-tight">
+          <span className="font-semibold text-xl tracking-tight mr-10">
             Tress-mesterskap
           </span>
         </a>
-        <div className="md:items-center md:w-auto flex">
-          <div className="md:flex hidden">
-            <a className="block mr-4" href="/players">
+        <button
+          onClick={handleClick}
+          className="focus:outline-none inline-flex p-2 hover:bg-yellow-100 rounded lg:hidden ml-auto hover:text-yellow-900 outline-none"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+
+        <div
+          className={`${
+            active ? "" : "hidden"
+          }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+        >
+          <div className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-yellow-600 hover:text-white">
+            <a className="block" href="/players">
               Spillere
             </a>
           </div>
-          <div className="md:flex hidden">
-            <a className="block mr-4" href="/match">
+          <div className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-yellow-600 hover:text-white">
+            <a className="block" href="/match">
               Slag
             </a>
           </div>
--- web/components/intro.js
+++ web/components/intro.js
@@ -2,13 +2,11 @@ import { CMS_NAME, CMS_URL } from "../lib/constants";
 
 export default function Intro() {
   return (
-    <section className="mt-16 mb-16 md:mb-12">
+    <section className="mt-6 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Sælens evige tressmesterskap
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5">
-        Det tar aldri slutt
-      </h4>
+      <h4 className="md:text-left text-lg mt-5">Det tar aldri slutt</h4>
     </section>
   );
 }
