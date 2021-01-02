bc319a34a613ad14f8ac74b0d19b850433e5290f
Tarje Lavik <tarje.lavik@gmail.com>
2021-01-02 21:26:19 +0100

    Fix name

--- web/components/footer.js
+++ web/components/footer.js
@@ -1,3 +1,4 @@
+import { CMS_NAME } from "../lib/constants";
 import Container from "./container";
 
 export default function Footer() {
@@ -6,7 +7,7 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-3xl lg:text-4xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Tress-mesterskap er tilknyttet{" "}
+            {CMS_NAME} er tilknyttet{" "}
             <strong>International Tress Association</strong>.
           </h3>
         </div>
--- web/components/intro.js
+++ web/components/intro.js
@@ -4,7 +4,7 @@ export default function Intro() {
   return (
     <section className="mt-6 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Vaksdalmesterskap i tress
+        {CMS_NAME}
       </h1>
       <h4 className="md:text-left text-lg mt-5">Det tar aldri slutt</h4>
     </section>
