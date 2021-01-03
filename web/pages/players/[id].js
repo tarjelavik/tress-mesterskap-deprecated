d75d15397763f82410833bd65ebf81949ba5b82f
Tarje Lavik <tarje.lavik@gmail.com>
2021-01-03 12:19:10 +0100

    Fix mobile view

--- web/pages/players/[id].js
+++ web/pages/players/[id].js
@@ -68,71 +68,16 @@ export default function Player({ player, preview }) {
 
               <div id="stats" className="max-w-xl px-4 py-4 mx-auto">
                 <div className="sm:grid sm:h-32 sm:grid-flow-row sm:gap-4 sm:grid-cols-3">
-                  <div className="flex flex-col justify-center px-4 py-4 bg-white border border-gray-300 rounded">
+                  <div className="px-4 py-4 mb-5 bg-white border border-gray-300 rounded">
                     <AverageScore player={player._id} games={player.games} />
                   </div>
-                  <div className="flex flex-col justify-center px-4 py-4 bg-white border border-gray-300 rounded">
+                  <div className="px-4 py-4 mb-5 bg-white border border-gray-300 rounded">
                     <MatchesWon player={player._id} games={player.games} />
                   </div>
-
-                  {/* <div
-                    className="flex flex-col justify-center px-4 py-4 mt-4 bg-white border border-gray-300 rounded sm:mt-0"
-                  >
-                    <div>
-                      <div>
-                        <p className="flex items-center justify-end text-red-500 text-md">
-                          <span className="font-bold">6%</span>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-5 h-5 fill-current"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              className="heroicon-ui"
-                              d="M20 9a1 1 0 012 0v8a1 1 0 01-1 1h-8a1 1 0 010-2h5.59L13 10.41l-3.3 3.3a1 1 0 01-1.4 0l-6-6a1 1 0 011.4-1.42L9 11.6l3.3-3.3a1 1 0 011.4 0l6.3 6.3V9z"
-                            />
-                          </svg>
-                        </p>
-                      </div>
-                      <p className="text-3xl font-semibold text-center text-gray-800">
-                        43
-                      </p>
-                      <p className="text-lg text-center text-gray-500">
-                        New Tickets
-                      </p>
-                    </div>
-                  </div>
-
-                  <div
-                    className="flex flex-col justify-center px-4 py-4 mt-4 bg-white border border-gray-300 rounded sm:mt-0"
-                  >
-                    <div>
-                      <div>
-                        <p className="flex items-center justify-end text-gray-500 text-md">
-                          <span className="font-bold">0%</span>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-5 h-5 fill-current"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              className="heroicon-ui"
-                              d="M17 11a1 1 0 010 2H7a1 1 0 010-2h10z"
-                            />
-                          </svg>
-                        </p>
-                      </div>
-                      <p className="text-3xl font-semibold text-center text-gray-800">
-                        43
-                      </p>
-                      <p className="text-lg text-center text-gray-500">
-                        New Tickets
-                      </p>
-                    </div>
-                  </div> */}
                 </div>
               </div>
-              <div className="grid grid-cols-2 gap-5">
+
+              <div className="grid md:grid-cols-2 gap-5">
                 <div className="my-10">
                   <h1 className="text-2xl font-bold text-center">
                     Resultat per spill
